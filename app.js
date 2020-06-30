26a53ff78bf45bb1647d6f535515d9c67c20324d
Yakima <cleveryun@163.com>
2020-06-30 15:44:32 +0800

    backup

--- app.js
+++ app.js
@@ -1,43 +1,44 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        appid: '',
-        appsecret: '',
-        specials: [],
-        searchResult: [
-            { id: '1', name: '长沙市周南中学', added: '1' },
-            { id: '2', name: '杭州市周南中学', added: '0' },
-            { id: '3', name: '温州市周南有限公司', added: '0' },
-        ],
-        // 已选商户
-        selectShops: [
-            { id: '1', name: '长沙市周南中学' },
-            { id: '2', name: '长沙市周南中学' },
-            { id: '3', name: '长沙市周南中学' },
-            { id: '4', name: '长沙市周南中学' },
-            { id: '5', name: '长沙市周南中学' },
-        ],
-    },
-    methods: {
-        toCopyText: function (copyText) {
-            console.log(copyText)
-        },
-        toAddShop: function (shopId) {
-            console.log(shopId)
-        },
-        toDeleteShop: function (shopId) {
-            console.log(shopId)
-        },
-    },
-    created: function () {
-        $('#app').addClass('show')
-    },
-})
-
 var form = layui.form;
 
-//监听提交
+// 渲染表单
+form.render() // 如果有动态通过js修改过dom结构，记得重新调用下form.render()
+
+// 渲染表单完毕后显示页面
+$('#app').addClass('show')
+
+// 重置字段
+$('.btnReset').click(function () {
+    const $this = $(this)
+    $this.parent().find('input').val('')
+})
+
+// 开启文本复制功能
+new ClipboardJS('.btnCopy')
+
+//监听表单提交
 form.on('submit(formDemo)', function(data){
+    // data.field里是表单里填写的数据
     layer.msg(JSON.stringify(data.field));
+    console.log(data)
+
+    // 在这里发送请求
+    $.ajax({
+        type: 'POST',
+        contentType: 'application/x-www-form-urlencoded;charset=utf-8',
+        url: '/apis/text-api.do',
+        dataType: 'json',
+        processData: true,
+        timeout: 20000,
+        data: {}, // 换成实际需要的参数
+        success: function (data, status, xhr) {
+            layer.msg('请求成功')
+        },
+        fail: function (xhr, errorType, error) {
+            layer.msg('请求失败')
+        },
+        complete: function (xhr, status) {
+            layer.msg('请求结束，不管是成功还是失败')
+        },
+    })
     return false;
 });
